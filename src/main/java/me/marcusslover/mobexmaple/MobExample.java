87773d0219213f00c671b542a610fec4c505c87b
MarcusSlover <mariuszek016@gmail.com>
2021-02-24 21:33:03 +0100

    + init

---
+++ src/main/java/me/marcusslover/mobexmaple/MobExample.java
@@ -0,0 +1,11 @@
+package me.marcusslover.mobexmaple;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MobExample extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
